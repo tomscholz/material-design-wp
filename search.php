9b8410e98601ee5cd47c7dc92db88d4d6043938f
Tom Scholz <tomscholz@users.noreply.github.com>
2018-10-01 12:40:13 +0200

    Update search.php

--- search.php
+++ search.php
@@ -1,13 +1,22 @@
-<?php get_header(); ?>
-	<main class="mdcwp-card__adjust mat-toolbar-adjust">
-		<h1 class="mdc-typography--display1">Search results for: <?php echo get_search_query(); ?></h1>
-		<?php 	
-		if(have_posts()):
-			while(have_posts()): the_post(); ?>
+<?php get_header(); ?>
 
-				<?php get_template_part('content'); ?>
-			<?php endwhile;
-		endif;
-		?>
-	</main>
+	<main class="mdcwp-card__adjust mat-toolbar-adjust">
+		<h1 class="mdc-typography--display1">Search results for: <?php echo get_search_query(); ?></h1>
+    
+		<?php
+    
+		if(have_posts()):
+    
+			while(have_posts()): the_post(); ?>
+    
+				<?php get_template_part('content'); ?>
+    
+			<?php endwhile;
+    
+		endif;
+    
+		?>
+    
+	</main>
+
 <?php get_footer(); ?>
